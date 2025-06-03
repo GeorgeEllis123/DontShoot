68855e343d0ab6dd9a6bfda4649611150748097c
george <70286749+GeorgeEllis123@users.noreply.github.com>
2025-06-03 15:30:14 -0400

    Basic clicking mechaninc implemented

---
+++ Assets/Scripts/CircleShrinking.cs
@@ -0,0 +1,27 @@
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class CircleShrinking : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float startingScale = 5f;
+
+    private float currentScale;
+
+    private void Start()
+    {
+        ResetCircle();
+    }
+
+    void Update()
+    {
+        currentScale -= speed * Time.deltaTime;
+        gameObject.transform.localScale = new Vector3(currentScale, currentScale, 0);
+    }
+
+    public void ResetCircle()
+    {
+        currentScale = startingScale;
+        gameObject.transform.localScale = new Vector3(startingScale, startingScale, 0);
+    }
+}
--- Assets/Scripts/ClickDetection.cs
+++ Assets/Scripts/ClickDetection.cs
@@ -2,15 +2,7 @@ using UnityEngine;
 
 public class ClickDetection : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
