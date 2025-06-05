aec9e44dd46b0aa66c946e82f418bbac1b0e9259
george <70286749+GeorgeEllis123@users.noreply.github.com>
2025-06-05 15:44:03 -0400

    Bobbing script in main menu

---
+++ Assets/Scripts/CameraBob.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CameraBob : MonoBehaviour
+{
+    [SerializeField] private float bobSpeed = 2;
+    [SerializeField] private float bobAmount = 2;
+
+    private int direction = 1;
+
+    private float startY;
+
+    private void Start()
+    {
+        startY = transform.localPosition.y;
+    }
+
+    private void Update()
+    {
+        if (transform.localPosition.y - startY > bobAmount || transform.localPosition.y - startY < 0) 
+        {
+            direction *= -1;
+        }
+        float newY = transform.localPosition.y + direction * Time.deltaTime * bobSpeed;
+
+        transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
+    }
+}
