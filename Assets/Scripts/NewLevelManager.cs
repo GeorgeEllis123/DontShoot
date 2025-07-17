c67ac35a47a6642195927e36897ebd1c4d767314
karysfontana <karysfontana@gmail.com>
2025-07-17 13:32:26 -0400

    achievements impl again
    uncomment and checked

--- Assets/Scripts/NewLevelManager.cs
+++ Assets/Scripts/NewLevelManager.cs
@@ -39,12 +39,12 @@ public class NewLevelManager : MonoBehaviour
     {
         highScore = PlayerPrefs.GetInt(highScoreKey, 0);
         currentDay = highScore;
-        // Unlock ach at beginning of day 1 
-        //if (day == 1)
-        //    AchievementManager.UnlockAchievement("ACH_COODNAPPED");
+        // Unlock ach at beginning of day 1 & 9
+        if (day == 1)
+           AchievementManager.UnlockAchievement("ACH_COODNAPPED");
 
-        //if (day == 9)
-        //    AchievementManager.UnlockAchievement("ACH_NO_MORE_COOS");
+        if (day == 9)
+            AchievementManager.UnlockAchievement("ACH_NO_MORE_COOS");
 
         StartCoroutine("Play");
     }
